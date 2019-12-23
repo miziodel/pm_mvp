a49198ae00c2bdc3da9c2202cee78c3d05e49352
Maurizio Delmonte <maurizio.delmonte@abstract.it>
2019-12-23 13:02:56 +0100

    updates: ADD: dependencies from OCA modules ADD: project log notes ADD: project budget hours, priority, task planned hours ADD: project mileston date start, budget hours, task planned hours ADD: milestones views (draft) UPDATE: projects list

--- __manifest__.py
+++ __manifest__.py
@@ -22,7 +22,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Project',
+    'category': 'Project Management',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,6 +32,10 @@
                 'project_list',
                 'project_status',
                 'project_timeline',
+                'project_description',
+                'project_key',
+                'project_milestone',
+                'project_parent_task_filter',
                 ],
 
     # always loaded
--- models/project.py
+++ models/project.py
@@ -5,10 +5,21 @@ from odoo import models, fields, api
 
 class Project(models.Model):
 
-    # XXX: _name is mandatory to let activity.mixin bootstrap correctly
     _name = 'project.project' 
-    _inherit = ['project.project', 'mail.activity.mixin']
+    _inherit = ['project.project', 'mail.thread', 'mail.activity.mixin']
     
+    project_budget_hours = fields.Integer(string="Project Budget Hours",
+                                help="agreed number of hours to complete the project.")
+    
+    task_planned_hours = fields.Integer(compute='_compute_tasks_planned_hours', 
+                                string="Planned Hours on total tasks",
+                                help="number of hours planned on the whole set of tasks.")
+    task_active_planned_hours = fields.Integer(compute='_compute_tasks_active_planned_hours', 
+                                string="Planned Hours on uncompleted tasks",
+                                help="number of hours planned on the tasks yet to be done.")
+    
+    priority = fields.Integer(string="Priority",
+                              help="priority number: higher the number, higher the priority.")
     
     @api.multi
     def open_project_view(self):
@@ -22,7 +33,8 @@ class Project(models.Model):
                 'res_model': 'project.project',
                 'views': [(False,'form')],
                 'type': 'ir.actions.act_window',
-                'target': 'new',
+                # 'target': 'new',
+                'target': 'current',
                 'res_id': self.id,
                 # if you want to open the form in edit mode direclty            
                 'flags': {'initial_mode': 'edit'},
@@ -46,3 +58,47 @@ class Project(models.Model):
                 'context': dict(self.env.context),
                 }
     
+    def _compute_tasks_planned_hours(self):
+        for project in self:
+            project.task_planned_hours = sum(project.task_ids.
+                                                mapped('planned_hours'))
+
+    def _compute_tasks_active_planned_hours(self):
+        active_tasks = lambda r: not r.stage_id.closed
+        for project in self:
+            project.task_active_planned_hours = sum(project.task_ids.
+                                                      filtered(active_tasks).
+                                                      mapped('planned_hours'))
+
+
+class ProjectMilestone(models.Model):
+    _inherit = 'project.milestone'
+      
+
+    date_start = fields.Datetime(string='Starting Date',
+                                 default=fields.Datetime.now,
+                                 index=True, copy=False)
+    
+    milestone_budget_hours = fields.Integer(string="Milestone Budget Hours",
+                                help="agreed number of hours to complete the milestone.")
+    
+    mls_task_planned_hours = fields.Integer(compute='_compute_mls_tasks_planned_hours', 
+                                string="Planned Hours on milestones' total tasks",
+                                help="number of hours planned on the whole set of tasks for the milestone.")
+    mls_task_active_planned_hours = fields.Integer(compute='_compute_mls_tasks_active_planned_hours', 
+                                string="Planned Hours on uncompleted tasks",
+                                help="number of hours planned on the tasks yet to be done for the milestone.")
+
+    def _compute_mls_tasks_planned_hours(self):
+        for milestone in self:
+            milestone.mls_task_planned_hours = sum(milestone.project_task_ids.
+                                                    mapped('planned_hours'))
+
+    def _compute_mls_tasks_active_planned_hours(self):
+        active_tasks = lambda r: not r.stage_id.closed
+        for milestone in self:
+            milestone.mls_task_active_planned_hours = sum(milestone.project_task_ids.
+                                                            filtered(active_tasks).
+                                                            mapped('planned_hours'))
+
+# TODO: mostra tempi pianificati e totali sulla grid delle milestone nella form view del progetto!!
